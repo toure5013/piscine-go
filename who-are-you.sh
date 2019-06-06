88e82276f0381b9bf4624e0735717c0a5417eb58
toure5013 <toure5013@gmail.com>
2019-06-06 12:13:00 +0000

    add my name

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-name=$(curl https://api.github.com/users/hverby | grep '"login":' | sed -e 's/^.": "//g' -e 's/",.$//g')
+name=$(curl https://api.github.com/users/toure5013 | grep '"login":' | sed -e 's/^.": "//g' -e 's/",.$//g')
 
 echo "nom: $name"
