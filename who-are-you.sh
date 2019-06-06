2bcc50f272f0a4249a9202d00933ed3055317d39
toure5013 <toure5013@gmail.com>
2019-06-06 12:15:01 +0000

    just get my naùe

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-name=$(curl https://api.github.com/users/toure5013 | grep '"login":' | sed -e 's/^.": "//g' -e 's/",.$//g')
-
-echo "nom: $name"
+name=$(curl https://api.github.com/users/toure5013 | grep '"login"' | cut -d : -f 2 | cut -d , -f1 | cut -d "" -f2)
+echo $name
