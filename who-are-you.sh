984b151036128600984c7a4e1e9bf56699b4e09b
toure5013 <toure5013@gmail.com>
2019-06-06 12:12:09 +0000

    get my name

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-name=$(curl https://api.github.com/users/toure5013 | grep '"name"' | cut -d : -f 2 | cut -d , -f1 | cut -d "" -f2)
-echo $name
+name=$(curl https://api.github.com/users/hverby | grep '"login":' | sed -e 's/^.": "//g' -e 's/",.$//g')
+
+echo "nom: $name"
