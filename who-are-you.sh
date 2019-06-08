b5172811c93df3dccf4b8cc7a506e4c8cfa9a837
toure5013 <toure5013@gmail.com>
2019-06-08 17:00:58 +0000

    look updated

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-name=$(curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[] | select(.id==70)'| .name')
+name=$(curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[] | select(.id==70) | .name')
 echo $name
