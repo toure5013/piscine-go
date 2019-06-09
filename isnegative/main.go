b6f82438860f6414914dabd573fbd8d455f5e745
toure5013 <toure5013@gmail.com>
2019-06-09 15:34:08 +0000

    isnagetive func

--- isnegative/main.go
+++ isnegative/main.go
@@ -1,13 +1,7 @@
 package main
 
 import "fmt"
-// import piscine ".."
 
-func main() {
-		IsNegative(1)
-		IsNegative(0)
-		IsNegative(-1)
-}
 // Écrire une fonction qui affiche 'T' (true) sur une seule ligne si l’int passé en
 //  paramètre est négatif, sinon elle affiche 'F' (false).
 
